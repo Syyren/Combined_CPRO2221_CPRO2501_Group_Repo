a8fb7978658eec926c8177f8b3d7ec05e2a92a28
Syyren <129496054+Syyren@users.noreply.github.com>
2024-02-28 17:09:51 -0700

    Established framework

---
+++ backend/src/main/java/com/brcg/coolcatgames/CoolcatgamesApplication.java
@@ -0,0 +1,13 @@
+package com.brcg.coolcatgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolcatgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolcatgamesApplication.class, args);
+	}
+
+}
