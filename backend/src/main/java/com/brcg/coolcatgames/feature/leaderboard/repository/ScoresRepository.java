d06dbb5eeaac0b4855e3e4ce78863b66e2f2ee86
NegrebLC <BergCu2001@gmail.com>
2024-03-05 11:24:09 -0700

    Public Abstract
    made scores entity public abstract

--- backend/src/main/java/com/brcg/coolcatgames/feature/leaderboard/repository/ScoresRepository.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/leaderboard/repository/ScoresRepository.java
@@ -2,6 +2,7 @@ package com.brcg.coolcatgames.feature.leaderboard.repository;
 
 
 
+import com.brcg.coolcatgames.feature.leaderboard.model.ScoreEntry;
 import com.brcg.coolcatgames.feature.leaderboard.service.ScoresService;
 import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,6 +11,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @Document(collection = "Scores")
-// add in the abstract extension before the comma!!!!!!!!
-public interface ScoresRepository extends MongoRepository<, Long> {
+public interface ScoresRepository extends MongoRepository<ScoreEntry, Long> {
 }
