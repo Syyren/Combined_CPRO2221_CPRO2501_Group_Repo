53125ead99be10e848746df45072d07dc0f28e60
Flabort <Joel.ganson@rdpolytech.ca>
2024-03-04 19:10:15 -0700

    Fixed RequestMapping missing from controller

--- backend/src/main/java/com/brcg/coolcatgames/feature/tictactoe/controller/GameInProgressController.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/tictactoe/controller/GameInProgressController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RestController("/tictactoe")
+@RestController
+@RequestMapping("/tictactoe")
 @CrossOrigin(origins = "http://localhost:3000")
 public class GameInProgressController {
     @Autowired
