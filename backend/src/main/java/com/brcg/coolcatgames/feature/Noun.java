d568d5be8e0dbcc67b139569adda9c4fefeda623
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-03-04 23:21:11 -0700

    added noun-list parser to obtain 1 at random, found compact version, removed setter as it was not used due to nature of getting noun

---
+++ backend/src/main/java/com/brcg/coolcatgames/feature/Noun.java
@@ -0,0 +1,30 @@
+package com.brcg.coolcatgames.feature;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class Noun {
+    private String noun;
+
+    public String getNoun() {
+        Path path = Paths.get("data/unique-noun-list.txt");
+        try (Stream<String> lines = Files.lines(path);
+             BufferedReader reader = new BufferedReader(Files.newBufferedReader(path))) {
+
+            long totalLines = lines.count();
+            Random random = new Random();
+            int lineNumber = random.nextInt((int) totalLines) + 1;
+            noun = reader.lines()
+                    .skip(lineNumber - 1)
+                    .findFirst()
+                    .orElse(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return noun;
+    }
+}
