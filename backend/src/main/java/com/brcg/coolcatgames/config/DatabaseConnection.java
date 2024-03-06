0d3e0bbda12f42249aec45254692b2a60017df87
Antoinette Afumwaa <129301965+annn167@users.noreply.github.com>
2024-03-06 13:14:34 -0700

    test comment

--- backend/src/main/java/com/brcg/coolcatgames/config/DatabaseConnection.java
+++ backend/src/main/java/com/brcg/coolcatgames/config/DatabaseConnection.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+//test
 @Configuration
 @EnableMongoRepositories(basePackages = "com.brcg.coolcatgames")
 public class DatabaseConnection extends AbstractMongoClientConfiguration {
