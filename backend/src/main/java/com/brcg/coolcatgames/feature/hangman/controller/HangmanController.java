5b4992206a88fb88aceb54c3890211b315e0da68
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-03-06 11:41:54 -0700

    added api's for retrieving gamestates and sending score

--- backend/src/main/java/com/brcg/coolcatgames/feature/hangman/controller/HangmanController.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/hangman/controller/HangmanController.java
@@ -1,15 +1,50 @@
 package com.brcg.coolcatgames.feature.hangman.controller;
 
 import com.brcg.coolcatgames.feature.hangman.model.Hangman;
+import com.brcg.coolcatgames.feature.hangman.model.HangmanGameState;
+import com.brcg.coolcatgames.feature.hangman.model.HangmanScore;
+import com.brcg.coolcatgames.feature.hangman.service.HangmanScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/hangman")
 public class HangmanController {
-    private Hangman hangman;
+    private final Hangman hangman = new Hangman();
+    private final HangmanScore hangmanScore = new HangmanScore();
+    private final HangmanScoreService hangmanScoreService;
 
-    public HangmanController() {
-        this.hangman = new Hangman();
+    @Autowired
+    public HangmanController(HangmanScoreService hangmanScoreService) {
+        this.hangmanScoreService = hangmanScoreService;
+    }
+    @GetMapping("/gamestate")
+    public HangmanGameState getGameState() {
+        return new HangmanGameState(
+                hangman.getDisplayedWord(),
+                hangman.getGuesses(),
+                hangman.getTotalScore()
+        );
+    }
+
+    @PostMapping("/guess")
+    public ResponseEntity<String> guessLetter(@RequestParam char letterGuessed) {
+        hangman.guessLetter(letterGuessed);
+        return ResponseEntity.ok("Guess successful");
+    }
+
+    @GetMapping("/score")
+    public int getTotalScore() {
+        return hangman.getTotalScore();
+    }
+
+    @PostMapping("/save-score")
+    public ResponseEntity<String> saveScore() {
+        hangmanScore.setGameName("hangman");
+        hangmanScore.setUserId("Kaden"); //logic later
+        hangmanScoreService.saveScore(hangmanScore);
+        return ResponseEntity.ok("Score saved successfully");
     }
 
 }
