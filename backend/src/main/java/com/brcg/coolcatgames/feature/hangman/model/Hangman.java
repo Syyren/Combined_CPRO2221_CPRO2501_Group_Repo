53ee5868c2605a04703f700d2454b16a0a2ff4f8
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-03-06 11:40:28 -0700

    working towards hangmanScore integration

--- backend/src/main/java/com/brcg/coolcatgames/feature/hangman/model/Hangman.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/hangman/model/Hangman.java
@@ -2,64 +2,124 @@ package com.brcg.coolcatgames.feature.hangman.model;
 
 import lombok.Data;
 
+import java.util.Arrays;
+
 @Data
 public class Hangman {
-    private int guesses;
     private char[] secretWord;
     private char[] displayedWord;
-    //private final Alphabet[] ALPHABET = Alphabet.values();
     private char letterGuessed;
+    private HangmanScore score = new HangmanScore();
 
+    //game values
+    private int guesses = 7;
+    private int scoreOnGuess = 5;
+    private int scoreOnWin = 50;
+
+    //constructor
+    public Hangman(){
+        getSecretWord();
+        setDisplayedWord();
+    }
 
     //using Noun class to generate secret word
     private char[] getSecretWord(){
-        //getting
         Noun noun = new Noun();
         String randomNoun = noun.getNoun().toUpperCase();
         return this.secretWord = randomNoun.toCharArray();
     }
+
     //generates displayed word based off of secret word obtained
-    public char[] getDisplayedWord() {
-        int secretWordLength = getSecretWord().length;
-        this.displayedWord = new char[secretWordLength];
+    public void setDisplayedWord() {
+        System.out.println("s word: "+ Arrays.toString(this.secretWord));
+        int secretWordLength = secretWord.length;
+        displayedWord = new char[secretWordLength];
         //adds an underscore for every letter, adds any non letters as their own chars. ie: hyphens and spaces persist
-        for (int i = 0; i < this.secretWord.length; i++) {
-            char letter = this.secretWord[i];
-            if (Character.isLetter(letter))
-                this.displayedWord[i] = '_';
-            else
-                this.displayedWord[i] = letter;
+        for (int i = 0; i < secretWord.length; i++) {
+            char letter = secretWord[i];
+            if (Character.isLetter(letter)){
+                displayedWord[i] = '_';
+            } else {
+                displayedWord[i] = letter;
+            }
         }
-        return this.displayedWord;
     }
 
     //handles logic upon picking letter
     public void guessLetter(char letterGuessed){
-        if (letterInSecretWord(letterGuessed))
+        if (letterInSecretWord(letterGuessed)) {
+            score.updateScore(scoreOnGuess);
             revealLetter(letterGuessed);
-        else
+        } else {
             wrongLetter();
+        }
     }
 
     //checks if the guessed letter is in the secret word
     private boolean letterInSecretWord(char letterGuessed){
-        for(char secretLetter:this.secretWord){
-            if(secretLetter == letterGuessed)
+        for(char secretLetter:secretWord){
+            if(secretLetter == letterGuessed) {
                 return true;
+            }
         }
         return false;
     }
+
     //swaps the underscores with the correctly guessed letter
-    private void revealLetter(char letterGuessed){
-        for (int i = 0; i < this.secretWord.length; i++) {
-            if (this.secretWord[i] == letterGuessed)
+    private void revealLetter(char letterGuessed) {
+        for (int i = 0; i < secretWord.length; i++) {
+            if (secretWord[i] == letterGuessed) {
                 this.displayedWord[i] = letterGuessed;
+            }
+        }
+
+        System.out.println("Updated displayedWord: " + String.valueOf(displayedWord));
+
+        if (String.valueOf(displayedWord).equals(String.valueOf(secretWord))) {
+            win();
         }
     }
 
     //decrements guesses remaining
     private void wrongLetter(){
         this.guesses--;
+        if (this.guesses == 0){
+            gameOver();
+        }
+    }
+
+    //when game is lost or refuse to continue
+    public void gameOver(){
+        score.resetScore();
+        newGame();
+    }
+
+    //when game is won
+    public void win(){
+        score.updateScore(scoreOnWin);
+        if(playAgain()){
+            newGame();
+        } else {
+            gameOver();
+        }
+    }
+
+    //prompt for replay
+    private boolean playAgain(){
+        //prompt here
+        return true;
+    }
+
+    //New Game +
+    public void newGame(){
+        this.guesses = 7;
+        getSecretWord();
+        setDisplayedWord();
+    }
+
+    //get total score
+    public int getTotalScore(){
+        return score.getScore();
     }
 
 }
