1a887d8a1b7d188ad4ff5b7b79ea7a30f4c3bf50
Syyren <129496054+Syyren@users.noreply.github.com>
2024-03-05 22:04:51 -0700

    Implemented validation class, started mongoDB implementation

--- backend/src/main/java/com/brcg/coolcatgames/feature/achievements/repository/AchievementRepository.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/achievements/repository/AchievementRepository.java
@@ -2,17 +2,20 @@ package com.brcg.coolcatgames.feature.achievements.repository;
 
 import com.brcg.coolcatgames.feature.achievements.model.Achievement;
 import com.brcg.coolcatgames.feature.achievements.utils.AchievementValidation;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class AchievementRepository
 {
+    private MongoRepository<Achievement, Integer> mongoRepository;
     //initializing the achievementId counter to track inputted ids in ascending order as they're entered automatically
-    private int achievementIdCounter = 1;
+    private int achievementIdCounter = getHighestAchievementID() + 1;
     //initializing a list to hold the achievement objects in
     private final AchievementValidation validate = new AchievementValidation();
     private final List<Achievement> list = new ArrayList<>();
@@ -20,16 +23,13 @@ public class AchievementRepository
     public List<Achievement> getAllAchievements()
     {
         // TODO: validate.List(list);
-        return list;
+        return mongoRepository.findAll();
     }
     //method that filters through the achievement list by id and returns either a match or null
     public Achievement findById(int id)
     {
-        Achievement achievement = list.stream()
-                .filter(a -> a.getAchievementId() == id)
-                .findAny()
-                .orElse(null);
-        // TODO: validate.Achievement(achievement, id);
+        Achievement achievement = mongoRepository.findById(id).orElse(null);
+        // TODO: validate.Achievement(achievement, id)
         return achievement;
     }
     //method that saves an achievement object to the list
@@ -63,5 +63,20 @@ public class AchievementRepository
         // TODO: validate Achievement(achievement, id) or something to that effect
         list.removeIf(a -> a.getAchievementId() == id);
         return "Achievement with id: " + id + " removed successfully.";
+    }
+
+    //function that gets the highest id in the db and pulls it
+    private int getHighestAchievementID()
+    {
+        List<Achievement> achievementList = getAllAchievements();
+        int highestAchievementId = 0;
+        Achievement maxAchievement = achievementList.stream()
+                .max((a1, a2) -> Integer.compare(a1.getAchievementId(), a2.getAchievementId()))
+                .orElse(null);
+        if (maxAchievement != null)
+        {
+            highestAchievementId = maxAchievement.getAchievementId();
+        }
+        return highestAchievementId;
     }
 }
--- backend/src/main/java/com/brcg/coolcatgames/feature/achievements/utils/AchievementValidation.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/achievements/utils/AchievementValidation.java
@@ -1,6 +1,27 @@
 package com.brcg.coolcatgames.feature.achievements.utils;
 
+import com.brcg.coolcatgames.feature.achievements.model.Achievement;
+
+import java.util.List;
+
 public class AchievementValidation
 {
-    //TODO: add all validation stuff.
+
+    public void String(String stringName, String stringContent)
+    {
+        if (stringContent == null || stringContent.isEmpty())
+        { throw new AchievementCustomException(stringName + " cannot be null or empty!"); }
+    }
+
+    public void List(List<Achievement> list)
+    {
+        if (list.isEmpty())
+        { throw new AchievementCustomException("No achievements in the database!"); }
+    }
+
+    public void Achievement(Achievement achievement, int id)
+    {
+        if(achievement == null)
+        { throw new AchievementCustomException("Achievement not found with id: " + id); }
+    }
 }
