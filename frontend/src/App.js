0c442197cd0ded0ba92ff06668a379dac9556904
Syyren <129496054+Syyren@users.noreply.github.com>
2024-03-06 17:14:07 -0700

    added achievements route and AchievementTest view for testing feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom' //importing the react router dom to set my routes
 import About from './pages/About'
 import Home from './pages/Home'
+import AchievementTest from './pages/AchievementTest'
 import NoPage from './pages/NoPage' //importing each of my pages
 
 export default function App() { //setting the app routes
@@ -11,6 +12,7 @@ export default function App() { //setting the app routes
           <Route index element={ <Home /> } />
           <Route path="/home" element={ <Home /> } /> {/* Setting a second home path for clean routing */}
           <Route path="/about" element={ <About /> } />
+          <Route path="/achievements" element={ <AchievementTest /> } />
           <Route path="*" element={ <NoPage /> } /> {/* All undefined routes will raise the 404 page here */}
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/pages/AchievementTest.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Home()
+{
+    return(
+        <Layout>
+            <h2 className="display-4 mb-4">Achievement Test Page</h2>
+        </Layout> 
+    )
+}
