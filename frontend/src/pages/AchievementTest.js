cfa6431a5213ddd063d0412a0b203071c86eeed7
Syyren <129496054+Syyren@users.noreply.github.com>
2024-03-07 15:12:02 -0700

    updated list to include game name

--- frontend/src/pages/AchievementTest.js
+++ frontend/src/pages/AchievementTest.js
@@ -86,6 +86,8 @@ export default function AchievementTest()
                             className="list-group-item d-flex justify-content-between align-items-center"
                             key={index}>
                                 {achievement.achievementTitle} - {achievement.achievementDescription}
+                                <br />
+                                From Game: {achievement.gameName}
                                 <button 
                                 type="button"
                                 className="btn btn-warning"
