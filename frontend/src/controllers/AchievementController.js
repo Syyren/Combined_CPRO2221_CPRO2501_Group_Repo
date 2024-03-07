da05d82f761a309d423009506f6d7144287e2c6f
Syyren <129496054+Syyren@users.noreply.github.com>
2024-03-07 11:29:33 -0700

    adjusted url

--- frontend/src/controllers/AchievementController.js
+++ frontend/src/controllers/AchievementController.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const url = "http://localhost:8090/achievement";
-const get = "/get";
+const API_URL = "http://localhost:8090/api/achievement";
 
 //function that pulls all categories from the database through the axios
 export const getAchievements = async () => 
 {
     try 
     {
-      const contacts = await axios.get(url + get + "all");
+      const contacts = await axios.get(`${API_URL}/get/all`);
       console.log("Achievements from API:", contacts);
       return contacts.map(achievement => //returns a list of categories with their attributes tied to values
       ({ 
