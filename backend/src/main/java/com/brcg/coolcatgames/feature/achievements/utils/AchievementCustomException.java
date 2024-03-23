4a073abc33ba5e6a3dec2f90d30d8926d3f124ea
Antoinette Afumwaa <129301965+annn167@users.noreply.github.com>
2024-03-23 00:27:26 -0600

    update

---
+++ backend/src/main/java/com/brcg/coolcatgames/feature/achievements/utils/AchievementCustomException.java
@@ -0,0 +1,6 @@
+package com.brcg.coolcatgames.feature.achievements.utils;
+
+public class AchievementCustomException extends RuntimeException
+{
+    public AchievementCustomException(String message) { super(message); }
+}
