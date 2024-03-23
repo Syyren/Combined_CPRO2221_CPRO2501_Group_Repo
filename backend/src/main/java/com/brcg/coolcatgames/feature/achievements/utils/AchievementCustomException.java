0ba04159981e663300c3e9982b1684c0e8a55ba3
Antoinette Afumwaa <129301965+annn167@users.noreply.github.com>
2024-03-23 10:30:57 -0600

    Delete backend directory

--- backend/src/main/java/com/brcg/coolcatgames/feature/achievements/utils/AchievementCustomException.java
+++
@@ -1,6 +0,0 @@
-package com.brcg.coolcatgames.feature.achievements.utils;
-
-public class AchievementCustomException extends RuntimeException
-{
-    public AchievementCustomException(String message) { super(message); }
-}
