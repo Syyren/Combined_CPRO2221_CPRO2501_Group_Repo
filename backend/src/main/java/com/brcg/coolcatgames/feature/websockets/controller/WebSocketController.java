50abead20cba0afdb208e262b29971f4ded93ec8
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-04-14 15:15:45 -0600

    controller cleanup to work with hangmna service, removing ununused methods for now. created basic skeleton of websocket controller to be used

---
+++ backend/src/main/java/com/brcg/coolcatgames/feature/websockets/controller/WebSocketController.java
@@ -0,0 +1,31 @@
+package com.brcg.coolcatgames.feature.websockets.controller;
+
+import com.brcg.coolcatgames.service.JsonSerializationService;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+// Define WebSocket endpoint
+@Controller
+public class WebSocketController {
+
+    private final JsonSerializationService jsonService = new JsonSerializationService();
+
+    @MessageMapping("/games/{gameName}/{roomId}")
+    @SendTo("/games/{gameName}/{roomId}")
+    public String handleGameMessage(@DestinationVariable String roomId, String gameStateJson, @PathVariable String gameName) {
+        String newGameStateJson = gameStateJson;
+        switch (gameName) {
+            case "hangman" ->
+                    newGameStateJson = jsonService.serializeObjectToJson();
+            case "tic-tac-toe" ->
+                    newGameStateJson = jsonService.serializeObjectToJson();
+            default ->
+                    newGameStateJson = jsonService.serializeObjectToJson();
+        };
+        return newGameStateJson;
+    }
+}
+
--- backend/src/main/java/com/brcg/coolcatgames/service/JsonSerializationService.java
+++ backend/src/main/java/com/brcg/coolcatgames/service/JsonSerializationService.java
@@ -21,19 +21,20 @@ public class JsonSerializationService {
         }
     }
 
-    public String wrapJson(String type, String payload) {
-        try {
-            //takes payload string and wraps in type as json and maps as "type":"type", "payload":{payload}
-            var wrapper = new java.util.HashMap<String, Object>();
-            wrapper.put("type", type);
-            wrapper.put("payload", payload);
-            //re-serialize the map to string and returns as wrapped json
-            String wrappedJson = objectMapper.writeValueAsString(wrapper);
-            System.out.println("Payload successfully wrapped to new Json:\n"+ wrappedJson);
-            return wrappedJson;
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+//
+//    public String wrapJson(String type, String payload) {
+//        try {
+//            //takes payload string and wraps in type as json and maps as "type":"type", "payload":{payload}
+//            var wrapper = new java.util.HashMap<String, Object>();
+//            wrapper.put("type", type);
+//            wrapper.put("payload", payload);
+//            //re-serialize the map to string and returns as wrapped json
+//            String wrappedJson = objectMapper.writeValueAsString(wrapper);
+//            System.out.println("Payload successfully wrapped to new Json:\n"+ wrappedJson);
+//            return wrappedJson;
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
 }
