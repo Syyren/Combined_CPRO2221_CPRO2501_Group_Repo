e58baee0e514a0729b1e04093e96acf2c3141242
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-14 12:08:28 -0600

    updating frontend for database connectivity

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,48 +1,80 @@
 import React from "react";
 import { Link } from "react-router-dom"; // Importing Link so that the navbar can navigate using routes
+import { useAuth } from "../context/AuthContext";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import "./NavBar.css";
 
 // This function creates the navbar for the layout and returns it
 export default function NavBar() {
+  const currentUser = useAuth();
+  
   return (
-    <nav className="navbar navbar-expand-lg bg-secondary" data-bs-theme="dark">
-      <div className="container-fluid">
-        <a className="navbar-brand">CoolCatGames.com</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target=".navbar-collapse"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-          <ul className="navbar-nav flex-grow-1">
-            <li className="nav-item">
-              <Link className="nav-link text-dark" to="/home">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-dark" to="/games">
-                Games
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-dark" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-dark" to="/leaderboard">
-                Leaderboard
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="/home">CoolCatGames.com</Navbar.Brand>
+      
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/home">Home</Nav.Link>
+          <Nav.Link as={Link} to="/games">Games</Nav.Link>
+          <Nav.Link as={Link} to="/leaderboard">Leaderboard</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+        </Nav>
+        <Nav>
+          <div className="d-flex align-items-center">
+            {currentUser && <Navbar.Text className="userWelcome px-3">Welcome, {currentUser.username}!</Navbar.Text>}
+          </div>
+          {currentUser ? (
+            <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+          ) : (
+            <>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/register">Register</Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    // <nav className="navbar navbar-expand-lg bg-secondary" data-bs-theme="dark">
+    //   <div className="container-fluid">
+    //     <a className="navbar-brand">CoolCatGames.com</a>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-bs-toggle="collapse"
+    //       data-bs-target=".navbar-collapse"
+    //       aria-controls="navbarSupportedContent"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+    //       <ul className="navbar-nav flex-grow-1">
+    //         <li className="nav-item">
+    //           <Link className="nav-link text-dark" to="/home">
+    //             Home
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link text-dark" to="/games">
+    //             Games
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link text-dark" to="/about">
+    //             About
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link text-dark" to="/leaderboard">
+    //             Leaderboard
+    //           </Link>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 }
--- frontend/src/controllers/AchievementController.js
+++ frontend/src/controllers/AchievementController.js
@@ -1,31 +1,54 @@
 import axios from "axios";
+import { getPlayerById } from "./PlayerController";
+import { useAuth } from "../context/AuthContext";
 
-const API_URL = "http://localhost:8090/api/achievement";
+const API_URL = "http://localhost:8090/api";
 
 //function that pulls all categories from the database through the axios
 export const getAchievements = async () => {
-  try {
-    const res = await axios.get(`${API_URL}/get/all`, {
-      headers: { Authorization: authToken },
+  const { currentUser } = useAuth();
+  try 
+  {
+    const res = await axios.get(`${API_URL}/achievement/get/all`, {
+      headers: { Authorization: `Bearer ${currentUser.jwt}` },
     });
     const achievements = res.data;
     console.log("Achievements from API:", achievements);
     return achievements;
-  } catch (err) {
+  } 
+  catch (err) 
+  {
     console.error("Error fetching achievements:", err); //outputs in the event of an error fetching the categories
     return [];
   }
 };
 
-// Temporary Function to satisfy httpbasic auth Spring Security
-const username = "john_doe";
-const password = "password123";
-const authToken = generateAuthToken(username, password);
-
-function generateAuthToken(username, password) {
-  // Encode the username and password with Base64
-  const authToken = btoa(`${username}:${password}`);
-
-  // Return the complete Authorization header value
-  return `Basic ${authToken}`;
+export const getUserAchievements = async (userId) => {
+  try
+  {
+    const player = await getPlayerById(userId);
+    return player.achievements;
+  }
+  catch (err)
+  {
+    console.error("Error fetching user's achievements:", err); //outputs in the event of an error fetching the categories
+    return [];
+  }
 }
+
+export const giveAchievement = async (userId, achievementId) => {
+  const { currentUser } = useAuth();
+  try 
+  {
+    const res = await axios.put(`${API_URL}/players/give-achievement/${userId}/${achievementId}`, {
+      headers: { Authorization: `Bearer ${currentUser.jwt}` },
+    });
+    const response = res.data;
+    console.log("Response from API:", response);
+  } 
+  catch (err) 
+  {
+    console.error("Error giving achievement:", err); //outputs in the event of an error fetching the categories
+    return [];
+  }
+};
