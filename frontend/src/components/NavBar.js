415f6e9b9b1b7faa5a1908d0846d0c9e2f045603
NegrebLC <BergCu2001@gmail.com>
2024-04-14 14:18:49 -0600

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,36 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Importing Link so that the navbar can navigate using routes
+import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./NavBar.css";
 
-// This function creates the navbar for the layout and returns it
 export default function NavBar() {
-  const currentUser = useAuth();
+  const { currentUser } = useAuth();
+  console.log(currentUser);
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/home">CoolCatGames.com</Navbar.Brand>
-      
+      <Navbar.Brand as={Link} to="/home">
+        CoolCatGames.com
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link as={Link} to="/home">Home</Nav.Link>
-          <Nav.Link as={Link} to="/games">Games</Nav.Link>
-          <Nav.Link as={Link} to="/leaderboard">Leaderboard</Nav.Link>
-          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/home">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/games">
+            Games
+          </Nav.Link>
+          <Nav.Link as={Link} to="/leaderboard">
+            Leaderboard
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
         </Nav>
         <Nav>
           <div className="d-flex align-items-center">
-            {currentUser && <Navbar.Text className="userWelcome px-3">Welcome, {currentUser.username}!</Navbar.Text>}
+            {currentUser && (
+              <Navbar.Text className="userWelcome px-3">
+                Welcome, {currentUser.username}!
+              </Navbar.Text>
+            )}
           </div>
           {currentUser ? (
-            <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+            <Nav.Link as={Link} to="/logout">
+              Logout
+            </Nav.Link>
           ) : (
             <>
-              <Nav.Link as={Link} to="/login">Login</Nav.Link>
-              <Nav.Link as={Link} to="/register">Register</Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/register">
+                Register
+              </Nav.Link>
             </>
           )}
         </Nav>
