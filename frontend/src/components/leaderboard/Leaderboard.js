6ba0c4fa7da254cb8915b455c337776b7c99acc4
NegrebLC <BergCu2001@gmail.com>
2024-04-14 15:06:27 -0600

    Update Leaderboard.js

--- frontend/src/components/leaderboard/Leaderboard.js
+++ frontend/src/components/leaderboard/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import LeaderboardNav from "./LeaderboardNav";
 import LeaderboardScore from "./LeaderboardScore";
 import {
@@ -11,36 +11,55 @@ import { useAuth } from "../../context/AuthContext";
 const Leaderboard = () => {
   const { currentUser } = useAuth();
   const [scores, setScores] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [activeGame, setActiveGame] = useState("canine_invaders");
   const [activeScoreType, setActiveScoreType] = useState("global");
 
   useEffect(() => {
-    fetchScores();
+    const fetchAndSetScores = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        let fetchedScores = [];
+        if (activeScoreType === "global") {
+          fetchedScores = await getScoresByGame(activeGame);
+        } else if (activeScoreType === "personal" && currentUser) {
+          fetchedScores = await getScoresByUserAndGame(
+            currentUser.id,
+            activeGame
+          );
+        }
+
+        // Fetch user names for all scores
+        const scoresWithUsernames = await Promise.all(
+          fetchedScores.map(async (score) => {
+            try {
+              const player = await getPlayerById(score.userId);
+              return { ...score, username: player.username };
+            } catch (error) {
+              console.error("Failed to fetch user name:", error);
+              return { ...score, username: "Unknown" };
+            }
+          })
+        );
+
+        setScores(scoresWithUsernames);
+      } catch (error) {
+        console.error("Error fetching scores:", error);
+        setError("Failed to fetch scores.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAndSetScores();
   }, [activeGame, activeScoreType, currentUser]);
 
-  const fetchScores = async () => {
-    try {
-      let fetchedScores = [];
-
-      if (activeScoreType === "global") {
-        fetchedScores = await getScoresByGame(activeGame);
-      } else if (activeScoreType === "personal") {
-        fetchedScores = await getScoresByUserAndGame(
-          currentUser.id,
-          activeGame
-        );
-      }
-
-      setScores(fetchedScores);
-    } catch (error) {
-      console.error("Error fetching scores:", error);
-    }
-  };
-
-  const filterScores = () => {
+  const filteredScores = useMemo(() => {
     const sortedScores = [...scores].sort((a, b) => b.score - a.score);
     return sortedScores.slice(0, 10);
-  };
+  }, [scores]);
 
   const handleGameChange = (game) => {
     setActiveGame(game);
@@ -50,37 +69,30 @@ const Leaderboard = () => {
     setActiveScoreType(scoreType);
   };
 
-  const getUserNameById = async (userId) => {
-    try {
-      const player = await getPlayerById(userId);
-      return player.username;
-    } catch (error) {
-      console.error("Error fetching username:", error);
-      return "Unknown";
-    }
-  };
-
-  const filteredScores = filterScores();
   const capitalizedActiveGame =
     activeGame.charAt(0).toUpperCase() + activeGame.slice(1);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div className="container mt-5">
-      <h3 className="display-4 mb-5">Leaderboard</h3>
-      <h4>{currentUser.username}</h4>
+      <h3 className="display-4 mb-5">
+        Leaderboard for {capitalizedActiveGame}
+      </h3>
       <LeaderboardNav
         onGameChange={handleGameChange}
         onScoreTypeChange={handleScoreTypeChange}
       />
-      <h4 className="display-6 mb-2">{capitalizedActiveGame}</h4>
       <div className="list-group">
         {filteredScores.map((score, index) => (
           <LeaderboardScore
             key={score.id}
             rank={index + 1}
-            name={score.userId}
+            name={score.username}
             score={score.score}
-            game={getUserNameById(score.userId)}
-            currentUser={currentUser.username}
+            game={capitalizedActiveGame}
+            currentUser={currentUser && currentUser.username}
           />
         ))}
       </div>
