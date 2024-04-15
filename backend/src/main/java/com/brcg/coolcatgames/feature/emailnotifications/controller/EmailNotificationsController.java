cbe970dbd182c6ed221ea22c9e53b47bbf859e6f
Flabort <Joel.ganson@rdpolytech.ca>
2024-04-14 20:44:32 -0600

    Prep to get a list of all users

--- backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
@@ -5,20 +5,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public class EmailNotificationsController {
     @Autowired
     private EmailNotificationsService emailService;
 
     // This should run every hour
-    @Scheduled(cron="O O * * * *")
+    @Scheduled(fixedRate = 3600000)
     public void performTask() {
-        // TODO replace false with a way to detect if there's changes in one of the database schemas
-        if (false) {
-            String user;
-            String subject;
-            emailService.sendEmail(user, subject,"");
+        ArrayList<String> users = new ArrayList<String>();
+        for (String user: users) {
+            // TODO replace false with a way to detect if there's changes in one of the database schemas
+            if (false) {
+                String subject = "test";
+                emailService.sendEmail(user, subject,"test");
+            }
         }
+
     }
 
 
