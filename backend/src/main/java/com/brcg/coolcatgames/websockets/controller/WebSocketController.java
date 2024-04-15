75f469b2a08367e67bd0cc90ae60d16ea8c3bc2a
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-04-14 18:39:08 -0600

    skeletoning websocket handling, will make service to handle specific logic after direction

--- backend/src/main/java/com/brcg/coolcatgames/websockets/controller/WebSocketController.java
+++ backend/src/main/java/com/brcg/coolcatgames/websockets/controller/WebSocketController.java
@@ -1,17 +1,31 @@
 package com.brcg.coolcatgames.websockets.controller;
+
+import com.brcg.coolcatgames.service.JsonSerializationService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
+// Define WebSocket endpoint
 @Controller
 public class WebSocketController {
 
-    @MessageMapping("/game/{roomId}")
-    @SendTo("/topic/game/{roomId}")
-    public String handleGameMessage(@DestinationVariable String roomId, String gameStateJson) {
+    private final JsonSerializationService jsonService = new JsonSerializationService();
 
-        return gameStateJson;
+    @MessageMapping("/games/{gameName}/{roomId}")
+    @SendTo("/games/{gameName}/{roomId}")
+    public String handleGameMessage(@DestinationVariable String roomId, String gameStateJson, @PathVariable String gameName) {
+        String newGameStateJson = gameStateJson;
+//        switch (gameName) {
+//            case "hangman" ->
+//                    newGameStateJson = jsonService.serializeObjectToJson();
+//            case "tic-tac-toe" ->
+//                    newGameStateJson = jsonService.serializeObjectToJson();
+//            default ->
+//                    newGameStateJson = jsonService.serializeObjectToJson();
+//        };
+        return newGameStateJson;
     }
 }
 
