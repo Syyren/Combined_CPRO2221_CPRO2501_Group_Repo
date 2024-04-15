0dba62c8740a74671d4c475e2236ffdfc1597201
Flabort <Joel.ganson@rdpolytech.ca>
2024-04-15 16:49:25 -0600

    Achievements are now checked for emails too

--- backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
@@ -1,5 +1,7 @@
 package com.brcg.coolcatgames.feature.emailnotifications.controller;
 
+import com.brcg.coolcatgames.feature.achievements.model.Achievement;
+import com.brcg.coolcatgames.feature.achievements.service.AchievementService;
 import com.brcg.coolcatgames.feature.leaderboard.model.ScoreEntry;
 import com.brcg.coolcatgames.feature.leaderboard.model.Scores;
 import com.brcg.coolcatgames.feature.leaderboard.service.ScoreEntryService;
@@ -7,6 +9,7 @@ import com.brcg.coolcatgames.feature.userRegistration.model.Player;
 import com.brcg.coolcatgames.feature.userRegistration.service.PlayerService;
 
 import com.brcg.coolcatgames.feature.emailnotifications.service.EmailNotificationsService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.validation.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,6 +22,7 @@ import java.util.List;
 
 @Component
 public class EmailNotificationsController {
+    // Link to each service that we want to email about
     @Autowired
     private EmailNotificationsService emailService;
     @Autowired
@@ -27,7 +31,13 @@ public class EmailNotificationsController {
     @Autowired
     private ScoreEntryService scoreEntryService;
 
+    @Autowired
+    private AchievementService achievementService;
+
+
+    // Memory since last email
     private Map<String,Integer> scoresInMemory = new HashMap<>();
+    private Map<String,ArrayList<Integer>> achievementsInMemory = new HashMap<>();
 
     // This should run every 12 hours
     @Scheduled(fixedRate = 43200000)
@@ -35,6 +45,8 @@ public class EmailNotificationsController {
         Iterable<Player> users = playerService.listAll();
         // a variable to replace the scoresInMemory at the end of this function
         Map<String, Integer> newScoresInMemory = new HashMap<>();
+        // a variable to replace the achievementsInMemory at the end of this function
+        Map<String,ArrayList<Integer>> newAchievementsInMemory = new HashMap<>();
 
         for (Player user: users) {
             // If any of the subloops adds to the email body, it will send this to true and send the email
@@ -65,19 +77,31 @@ public class EmailNotificationsController {
                 newScoresInMemory.put(score.getGameName()+"_"+user.getId(),score.getScore());
             }
 
-            // TODO make this a loop, replace False with a check if an achievement was gained
-            if (false) {
-                EmailContent += "You achieved x achievement!\n";
-                sendMail = true;
+            // Check if the user has gained a new achievement since the last email.
+            List<Achievement> achievementList = achievementService.getAllAchievements();
+            for(Achievement achievement: achievementList) {
+                if (achievementsInMemory.containsKey(user.getId())) {
+                    ArrayList<Integer> lastRememberedAchievements = achievementsInMemory.get(user.getId());
+                    Integer achievementId = achievement.getAchievementId();
+                    if (user.getAchievements().contains(achievementId) && !lastRememberedAchievements.contains(achievementId)) {
+                        EmailContent += "You achieved x achievement!\n";
+                        sendMail = true;
+                    }
+                }
+
             }
+            // Since the user information stores all the achievements they have, I don't need to store a key for each user and achievement
+            newAchievementsInMemory.put(user.getId(),user.getAchievements());
             // Send the email
             if (sendMail) {
                 EmailContent += "\n\nThanks for being a part of CoolCatGames Community!";
-                String subject = "test";
+                String subject = "An update from CoolCatGames.com!";
                 emailService.sendEmail(user.getEmail(), subject,EmailContent);
             }
-            scoresInMemory = newScoresInMemory;
+
         }
+        scoresInMemory = newScoresInMemory;
+        achievementsInMemory = newAchievementsInMemory;
 
     }
 
