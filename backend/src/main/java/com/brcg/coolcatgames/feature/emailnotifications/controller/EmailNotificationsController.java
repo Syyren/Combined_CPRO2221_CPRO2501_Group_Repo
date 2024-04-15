313b4f9463c1e3689b480182518001edd4a695ab
Flabort <Joel.ganson@rdpolytech.ca>
2024-04-14 20:53:52 -0600

    get list of all players

--- backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
@@ -1,5 +1,8 @@
 package com.brcg.coolcatgames.feature.emailnotifications.controller;
 
+import com.brcg.coolcatgames.feature.userRegistration.model.Player;
+import com.brcg.coolcatgames.feature.userRegistration.service.PlayerService;
+
 import com.brcg.coolcatgames.feature.emailnotifications.service.EmailNotificationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,16 +14,19 @@ import java.util.ArrayList;
 public class EmailNotificationsController {
     @Autowired
     private EmailNotificationsService emailService;
+    @Autowired
+    private PlayerService playerService;
 
     // This should run every hour
     @Scheduled(fixedRate = 3600000)
     public void performTask() {
-        ArrayList<String> users = new ArrayList<String>();
-        for (String user: users) {
+        Iterable<Player> users = playerService.listAll();
+
+        for (Player user: users) {
             // TODO replace false with a way to detect if there's changes in one of the database schemas
             if (false) {
                 String subject = "test";
-                emailService.sendEmail(user, subject,"test");
+                emailService.sendEmail(user.getEmail(), subject,"test");
             }
         }
 
