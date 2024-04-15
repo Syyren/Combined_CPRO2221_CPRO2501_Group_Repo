9ffad528a6a78f2b7edb153ec64276abff79ab27
Flabort <Joel.ganson@rdpolytech.ca>
2024-04-14 21:00:18 -0600

    set up to collate multiple events in one email

--- backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/controller/EmailNotificationsController.java
@@ -4,6 +4,7 @@ import com.brcg.coolcatgames.feature.userRegistration.model.Player;
 import com.brcg.coolcatgames.feature.userRegistration.service.PlayerService;
 
 import com.brcg.coolcatgames.feature.emailnotifications.service.EmailNotificationsService;
+import jakarta.validation.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,16 +18,29 @@ public class EmailNotificationsController {
     @Autowired
     private PlayerService playerService;
 
-    // This should run every hour
-    @Scheduled(fixedRate = 3600000)
+    // This should run every 2 hours
+    @Scheduled(fixedRate = 7200000)
     public void performTask() {
         Iterable<Player> users = playerService.listAll();
 
         for (Player user: users) {
+            Boolean sendMail = false;
+            String EmailContent = "Dear " + user.getFirstName() +",\n\n";
+
             // TODO replace false with a way to detect if there's changes in one of the database schemas
             if (false) {
+                EmailContent += "x has beaten your high score in y\n";
+                sendMail = true;
+            }
+            if (false) {
+                EmailContent += "";
+                sendMail = true;
+            }
+
+            if (sendMail) {
+                EmailContent += "\n\nThanks for being a part of CoolCatGames Community!";
                 String subject = "test";
-                emailService.sendEmail(user.getEmail(), subject,"test");
+                emailService.sendEmail(user.getEmail(), subject,EmailContent);
             }
         }
 
