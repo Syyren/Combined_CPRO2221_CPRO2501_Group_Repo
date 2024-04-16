9713c56c5028aa032d526d3dec3b8afe697ca09c
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-15 23:19:23 -0600

    Implemented controller tests

--- backend/src/test/java/com/brcg/coolcatgames/autorunner_test/AutoRunnerControllerTest.java
+++ backend/src/test/java/com/brcg/coolcatgames/autorunner_test/AutoRunnerControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 
 public class AutoRunnerControllerTest {
 
