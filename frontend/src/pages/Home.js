d80ab710a3004cf22baae395d766fa9dd3c7ab64
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-16 19:15:34 -0600

    shrunk home page card

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,15 @@
 import Layout from "../components/Layout";
 import { Link } from "react-router-dom";
+import CoolCatLogo from "../images/home/coolcatlogo.png";
 import CoolCatImage from "../images/home/TheCoolCat.jpg";
-import FrogSpin from "../images/home/frog-spin-frog.gif"
+import FrogSpin from "../images/home/frog-spin-frog.gif";
 
 export default function Home() {
   return (
     <Layout>
-      <div className="homePageContainer mt-5">
-        <h2 className="display-4 mb-4">Welcome to CoolCatGames.com!</h2>
-        <div className="card">
+      <div className="homePageContainer d-flex flex-column align-items-center justidy-content-center mt-5">
+        <h2 className="display-4 mb-4">Welcome to <img className="homeLogo" src={CoolCatLogo} alt="coolcatgames.com"></img></h2>
+        <div className="card contentWrap">
           <img src={CoolCatImage} alt="a cat staring at the sky" className="card-img-top"></img>
           <div className="card-body">
             <h2 className="card-title display-4 mb-2">Why join up?</h2>
