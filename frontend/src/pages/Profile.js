6b9c1785e4e38e586f17d84aafbbf5a8bbad171a
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-17 14:39:15 -0600

    updated profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -8,24 +8,24 @@ export default function Profile() {
   const username = currentUser.username;
   return (
     <Layout>
-      <div className="d-flex mt-5 align-itmes-center justify-content-evenly">
-        <div className="">
-          <h2 className="display-4 mb-4">{username}'s Profile</h2>
-          <div>
-            <AchievementList />
-          </div>
-        </div>
+        <h2 className="display-4 mt-5 mb-4">{username}'s Profile</h2>
+        <div className="d-flex mt-5 align-itmes-center justify-content-evenly">
+            <div className="">
+                <div>
+                    <AchievementList />
+                </div>
+            </div>
 
         {/* Friends List */}
-        <div className="col-md-4 mb-3">
-          <div className="card h-100 shadow-sm">
-            <div className="card-body">
-              <h5 className="card-title">My Friends</h5>
-              <FriendsList />
+            <div className="col-md-4 mb-3">
+                <div className="card h-100 shadow-sm">
+                    <div className="card-body">
+                        <h5 className="card-title">My Friends</h5>
+                        <FriendsList />
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </Layout>
   );
 }
