ff02d710ca668e6032de4af24145a4aecc3f986f
Antoinette Afumwaa <129301965+annn167@users.noreply.github.com>
2024-04-22 13:34:46 -0600

    Update PlayerRepository.java
    for software patches

--- backend/src/main/java/com/brcg/coolcatgames/feature/userRegistration/repository/PlayerRepository.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/userRegistration/repository/PlayerRepository.java
@@ -3,7 +3,17 @@ package com.brcg.coolcatgames.feature.userRegistration.repository;
 import com.brcg.coolcatgames.feature.userRegistration.model.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PlayerRepository extends MongoRepository <Player, String>{
+/**
+ * Repository interface for Player entities.
+ */
+public interface PlayerRepository extends MongoRepository<Player, String> {
+    /**
+     * Retrieves a player by their username.
+     *
+     * @param userName the username of the player
+     * @return the player with the specified username, or null if not found
+     */
     Player findByUsername(String userName);
-}
 
+    Player findByEmail(String email);
+}
