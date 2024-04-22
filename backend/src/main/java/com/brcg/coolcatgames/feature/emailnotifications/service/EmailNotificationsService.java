3fcb3af74cc94cd4c84dcf8c5308e2c0abfb73b7
Flabort <Joel.ganson@rdpolytech.ca>
2024-04-21 18:29:53 -0600

    Changed accounts from gmail to outlook, different error now

--- backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/service/EmailNotificationsService.java
+++ backend/src/main/java/com/brcg/coolcatgames/feature/emailnotifications/service/EmailNotificationsService.java
@@ -1,12 +1,45 @@
 package com.brcg.coolcatgames.feature.emailnotifications.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
 
 @Service
 public class EmailNotificationsService {
+    @Value("${spring.mail.host}")
+    private String Host;
+    @Value("${spring.mail.username}")
+    private String Username;
+    @Value("${spring.mail.password}")
+    private String Password;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+
+        mailSender.setHost(Host);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(Username);
+        mailSender.setPassword(Password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
     private static  EmailNotificationsService instance;
     private EmailNotificationsService(){};
 
@@ -20,14 +53,12 @@ public class EmailNotificationsService {
         }
         return instance;
     }
-    @Autowired
-    private JavaMailSender javaMailSender;
 
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        javaMailSender.send(message);
+        getJavaMailSender().send(message);
     }
 }
